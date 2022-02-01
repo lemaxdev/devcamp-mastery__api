a0e1b283c39fec7f5fe70f55c0e0be2a89a9adae
lemaxdev <lemaxdev@pm.me>
2022-02-02 00:34:45 +0200

    Add logger + database connection

--- controllers/bootcamps.controller.js
+++ controllers/bootcamps.controller.js
@@ -1,23 +1,24 @@
+// @desc    Controllers for CRUD operations on bootcamps
 const Bootcamps = {
     // Retrieve all the bootcamps | GET /api/v1/bootcamps | Public
-    getAll: (req, res, next) => {
-        res.status(200).send({ status: true, msg: `Get all the bootcamps` });
+    getAll: (req, res) => {
+        res.status(200).send({ success: true, msg: `Get all the bootcamps` });
     },
     // Retrieve a bootcamp by ID | GET /api/v1/bootcamps/:id | Public 
-    getById: (req, res, next) => {
-        res.status(200).send({ status: true, msg: `Get the bootcamp with id ${req.params.id}` });
+    getById: (req, res) => {
+        res.status(200).send({ success: true, msg: `Get the bootcamp with id ${req.params.id}` });
     },
     // Add a new bootcamp | POST /api/v1/bootcamps | Privat
-    create: (req, res, next) => {
-        res.status(201).send({ status: true, msg: `Add a new bootcamp` });
+    create: (req, res) => {
+        res.status(201).send({ success: true, msg: `Add a new bootcamp` });
     },
     // Update a bootcamp by ID | PUT /api/v1/bootcamps/:id | Privat 
-    update: (req, res, next) => {
-        res.status(200).send({ status: true, msg: `Update the bootcamp with id ${req.params.id}` });
+    update: (req, res) => {
+        res.status(200).send({ success: true, msg: `Update the bootcamp with id ${req.params.id}` });
     },
     // Delete a bootcamp by ID | DELETE /api/v1/bootcamps/:id | Privat 
-    delete: (req, res, next) => {
-        res.status(200).send({ status: true, msg: `Delete the bootcamp with id ${req.params.id}` });
+    delete: (req, res) => {
+        res.status(200).send({ success: true, msg: `Delete the bootcamp with id ${req.params.id}` });
     }
 }
 
--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const Express = require('express');
 const App = Express();
 
 const ENV = require('./config/env.config');
+const ConnectDB = require('./config/db.config');
+const Logger = require('morgan');
+const BootcampsRouter = require('./routes/bootcamps');
+
+// Connection to the database
+ConnectDB();
+
+// Middleware for logging on requests, only for DEV
+if (ENV.NODE_ENV === 'development') {
+    App.use(Logger('dev'));
+}
+
+App.use('/api/v1/bootcamps', BootcampsRouter);
 
 App.listen(ENV.PORT, () => {
     console.log(`Server is up and running!`);
