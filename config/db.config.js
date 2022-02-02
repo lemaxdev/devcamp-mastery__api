e4ec2e3e6f38e793a5745f56374a82bb65a04960
lemaxdev <lemaxdev@pm.me>
2022-02-02 20:40:01 +0200

    Implement a handler for unhandled promise rejections + apply colors.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
+const Colors = require('colors');
+const Mongoose = require('mongoose');
 
 const ENV = require('./env.config');
 
-const DBConnect = async () => {
-    const conn = await mongoose.connect(ENV.DB_URI, {
+const ConnectDB = async () => {
+    const conn = await Mongoose.connect(ENV.DB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
 
-    console.log(`Connected to the database | ${conn.connection.host}`);
+    console.log(`Connected to the database | ${conn.connection.host}`.yellow);
 }
 
-module.exports = DBConnect
+module.exports = ConnectDB
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const Express = require('express');
 const App = Express();
+const Colors = require('colors');
 
 const ENV = require('./config/env.config');
 const ConnectDB = require('./config/db.config');
 const Logger = require('morgan');
 const BootcampsRouter = require('./routes/bootcamps');
 
-// Connection to the database
-ConnectDB();
-
 // Middleware for logging on requests, only for DEV
 if (ENV.NODE_ENV === 'development') {
     App.use(Logger('dev'));
 }
 
+// Mount router for <bootcamps> data
 App.use('/api/v1/bootcamps', BootcampsRouter);
 
-App.listen(ENV.PORT, () => {
-    console.log(`Server is up and running!`);
-    console.log(`MODE: ${ENV.NODE_ENV} | PORT: ${ENV.PORT}\n====================`);
+const Server = App.listen(ENV.PORT, async () => {
+    await ConnectDB(); // Try to connect to the database
+    // Launch the server
+    console.log(`API Server is up and running!`.green);
+    console.log(`MODE: ${ENV.NODE_ENV} | PORT: ${ENV.PORT}\n====================`.green);
+})
+
+// Handle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Can't launch the server due to an unexpected ERROR:`.magenta + `\n ${err}`.white);
+    Server.close(() => process.exit(1));
 })
