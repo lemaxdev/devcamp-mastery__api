4968faf33abed9abb080381926cdac239c19328a
lemaxdev <lemaxdev@pm.me>
2022-02-06 15:50:46 +0200

    Finished the bootcamps controller functionalities

--- controllers/bootcamps.controller.js
+++ controllers/bootcamps.controller.js
@@ -1,25 +1,75 @@
 // @desc    Controllers for CRUD operations on bootcamps
-const Bootcamps = {
-    // Retrieve all the bootcamps | GET /api/v1/bootcamps | Public
-    getAll: (req, res) => {
-        res.status(200).send({ success: true, msg: `Get all the bootcamps` });
-    },
-    // Retrieve a bootcamp by ID | GET /api/v1/bootcamps/:id | Public 
-    getById: (req, res) => {
-        res.status(200).send({ success: true, msg: `Get the bootcamp with id ${req.params.id}` });
-    },
-    // Add a new bootcamp | POST /api/v1/bootcamps | Privat
-    create: (req, res) => {
-        res.status(201).send({ success: true, msg: `Add a new bootcamp` });
-    },
-    // Update a bootcamp by ID | PUT /api/v1/bootcamps/:id | Privat 
-    update: (req, res) => {
-        res.status(200).send({ success: true, msg: `Update the bootcamp with id ${req.params.id}` });
-    },
-    // Delete a bootcamp by ID | DELETE /api/v1/bootcamps/:id | Privat 
-    delete: (req, res) => {
-        res.status(200).send({ success: true, msg: `Delete the bootcamp with id ${req.params.id}` });
-    }
-}
+const Bootcamp = require("../models/Bootcamp");
+const handleAsync = require('../middleware/async');
+const CustomError = require('../utils/customError');
 
-module.exports = Bootcamps
+const bootcamps = {
+    // Retrieve all the bootcamps | GET /api/v1/bootcamps | Public
+    getAll: handleAsync(async (req, res, next) => {
+        const bootcampList = await Bootcamp.find();
+
+        res.status(200).json({
+            success: true,
+            count: bootcampList.length,
+            bootcamps: bootcampList
+        })
+    }),
+
+    // Retrieve a bootcamp by ID | GET /api/v1/bootcamps/:id | Public
+    getById: handleAsync(async (req, res, next) => {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if (!bootcamp) {
+            return next(new CustomError(`BOOTCAMP not found with ID: ${req.params.id}`, 404));
+        }
+
+        res.status(200).json({
+            success: true,
+            bootcamp: bootcamp
+        })
+    }),
+
+    // Add a new bootcamp | POST /api/v1/bootcamps | Privat
+    create: handleAsync(async (req, res, next) => {
+        const bootcamp = await Bootcamp.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            new_added_bootcamp: bootcamp
+        });
+        console.log(bootcamp);
+    }),
+
+    // Update a bootcamp by ID | PUT /api/v1/bootcamps/:id | Privat
+    update: handleAsync(async (req, res, next) => {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!bootcamp) {
+            return next(new CustomError(`BOOTCAMP not found with ID: ${req.params.id}`, 404));
+        }
+
+        res.status(200).json({
+            success: true,
+            bootcamp_updated: bootcamp
+        });
+    }),
+
+    // Delete a bootcamp by ID | DELETE /api/v1/bootcamps/:id | Privat
+    delete: handleAsync(async (req, res, next) => {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+        if (!bootcamp) {
+            return next(new CustomError(`BOOTCAMP not found with ID: ${req.params.id}`, 404));
+        }
+
+        res.status(200).json({
+            success: true,
+            bootcamp_deleted: bootcamp
+        })
+    })
+};
+
+module.exports = bootcamps;
