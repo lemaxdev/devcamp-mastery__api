f813922feea23e24f0a856b1aea0b34e3d1516de
lemaxdev <lemaxdev@pm.me>
2022-02-08 23:15:24 +0200

    Mount Courses routes + handle invalid routes + typo

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
-const Express = require("express");
-const App = Express();
-const Colors = require("colors");
+const express = require("express");
+const colors = require("colors");
 
 const ENV = require("./config/env.config");
 const ConnectDB = require("./config/db.config");
 const Logger = require("morgan");
+
 const errorHandler = require('./middleware/errors');
-const BootcampsRouter = require("./routes/bootcamps");
+
+const bootcampsRouter = require("./routes/bootcamps");
+const coursesRouter = require('./routes/courses');
+
+const api = express();
 
 // Middleware for logging on requests, only for DEV
 if (ENV.NODE_ENV === "development") {
-    App.use(Logger("dev"));
+    api.use(Logger("dev"));
 }
 
 // Built-in middleware for body parsing JSON Content-Type
-App.use(Express.json());
+api.use(express.json());
 
-// Mount router for <bootcamps> data
-App.use("/api/v1/bootcamps", BootcampsRouter);
+// Mount routers
+api.use("/api/v1/bootcamps", bootcampsRouter);
+api.use('/api/v1/courses', coursesRouter);
+
+// Handle unmatched / invalid routes
+api.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        error: `Invalid request ${req.method} on route ${req.originalUrl}`
+    })
+})
 
 // Middleware for handling errors
-App.use(errorHandler);
+api.use(errorHandler);
 
-const Server = App.listen(ENV.PORT, async () => {
+const server = api.listen(ENV.PORT, async () => {
     await ConnectDB(); // Try to connect to the database
     // Launch the server
     console.log(`API Server is up and running!`.cyan.inverse);
@@ -31,6 +44,6 @@ const Server = App.listen(ENV.PORT, async () => {
 
 // Handle promise rejections
 process.on("unhandledRejection", (err, promise) => {
-    console.log(`Can't launch the server due to unexpected ERROR:`.magenta +`\n ${err}`.white); 
-    Server.close(() => process.exit(1));
+    console.log(`Can't launch the server due to unexpected ERROR:`.magenta + `\n ${err}`.white);
+    server.close(() => process.exit(1));
 });
