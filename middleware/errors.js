0f2e856249c823a4469e94779ba46461f203a59a
lemaxdev <lemaxdev@pm.me>
2022-02-09 10:29:14 +0200

    Changed the message response on handling CastErrors + typo

--- middleware/errors.js
+++ middleware/errors.js
@@ -1,8 +1,7 @@
 const CustomError = require('../utils/customError');
 
 const errorHandler = (err, req, res, next) => {
-    console.log(err);
-    // // Mongoose ValidationError
+    // Mongoose ValidationError
     if (err.name === 'ValidationError') {
         const msg = Object.values(err.errors).map(val => val.message);
         err = new CustomError(`Validation FAIL > ${msg}`, 400);
@@ -10,19 +9,13 @@ const errorHandler = (err, req, res, next) => {
 
     // Mongoose Duplicate key error
     if (err.code == 11000) {
-        // const key = Object.values(err.keyValue);
-        err = new CustomError(`Duplicate field value for 'name = ${err.keyValue.name}'`, 400);
+        const field = Object.keys(err.keyValue);
+        err = new CustomError(`Duplicate field value for <${field}>`, 400);
     }
 
-    // Mongoose bad _id format
+    // Mongoose CastErrors for bad type format of values
     if (err.name === 'CastError') {
-        // Mongoose asertion error > cast to Number failed
-        if (err.kind === 'Number') {
-            err = new CustomError(`Wrong format of Number params`, 400);
-        } else {
-            const model = err.message.split('model ')[1];
-            err = new CustomError(`Not found any ${model.toUpperCase()} with ID: ${err.value}`, 404);
-        }
+        err = new CustomError(`Wrong type for value ${err.stringValue}(type ${err.valueType}), require type <${err.kind}>`, 400);
     }
 
     // Send the response after handling the error
