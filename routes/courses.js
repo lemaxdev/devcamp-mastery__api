88d3bb84b555de7da032b2fba6248459ec1dd293
lemaxdev <lemaxdev@pm.me>
2022-02-10 10:11:11 +0200

    Populate courses list with related bootcamps

--- routes/courses.js
+++ routes/courses.js
@@ -7,7 +7,10 @@ const Course = require('../models/Course');
 const router = express.Router();
 
 router.route('/')
-    .get(filterResults(Course), courses.getAll)
+    .get(filterResults(Course, {
+        path: 'bootcamp',
+        select: 'name description'
+    }), courses.getAll)
     .post(courses.create);
 
 router.route('/bootcamp/:bootcampId').get(courses.getByBootcampId);
