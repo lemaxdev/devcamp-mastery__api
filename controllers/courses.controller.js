3bb8b23c242765da357428d2112efa04560eec43
lemaxdev <lemaxdev@pm.me>
2022-02-16 20:37:19 +0200

    Ownership and relationship between `User` and `Course`

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -2,6 +2,7 @@
 const Course = require('../models/Course');
 const handleAsync = require('../middleware/async');
 const CustomError = require('../utils/customError');
+const Bootcamp = require('../models/Bootcamp');
 
 const courses = {
     // Fetch all courses | GET /api/v1/courses | Public
@@ -23,25 +24,42 @@ const courses = {
     }),
 
     // Add a new course | POST  /api/v1/courses | Privat
-    create: handleAsync(async (req, res) => {
+    create: handleAsync(async (req, res, next) => {
+        const bootcamp = await Bootcamp.findById(req.body.bootcamp);
+        if (!bootcamp) {
+            return next(new CustomError('BOOTCAMP not found, cannot create a course to a Bootcamp does not exist', 404));
+        }
+
+        // Check user ownership of the bootcamp
+        if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return next(new CustomError('The user is not authorized to create a course for this bootcamp', 403));
+        }
+
+        req.body.user = req.user.id;
         const course = await Course.create(req.body);
 
         res.status(200).json({
             success: true,
             body: course
-        })
+        });
     }),
 
     // Update a course | PUT  /api/v1/courses/:id | Privat
     update: handleAsync(async (req, res, next) => {
-        const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
+        let course = await Course.findById(req.params.id);
         if (!course) {
             return next(new CustomError('COURSE not found', 404));
         }
 
+        // Check user ownership of the course
+        if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return next(new CustomError('The user is not authorized to update this course', 403));
+        }
+
+        course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
         res.status(200).json({
             success: true,
             body: course
@@ -54,8 +72,13 @@ const courses = {
         if (!course) {
             return next(new CustomError('COURSE not found', 404));
         }
-        course.remove();
 
+        // Check user ownership of the course
+        if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return next(new CustomError('The user is not authorized to delete this course', 403));
+        }
+
+        course.remove();
         res.status(200).json({
             success: true,
             body: course
--- models/Course.js
+++ models/Course.js
@@ -42,6 +42,12 @@ const CourseSchema = new mongoose.Schema({
         type: mongoose.Schema.ObjectId,
         ref: 'Bootcamp',
         required: true,
+    },
+
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
     }
 });
 
