e5de46a09288ae30e3be5d834989038ec3f61c51
Alexandru Lemnean <lemaxdev@pm.me>
2023-11-14 19:48:05 +0000

    🛠fix: remove `mongoose.connect` options to fix the MONGODB DRIVER warnings

--- config/db.config.js
+++ config/db.config.js
@@ -5,10 +5,7 @@ const ENV = require('./env.config');
 
 async function connectDB() {
     mongoose.set('strictQuery', true);
-    const conn = await mongoose.connect(ENV.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(ENV.DB_URI);
 
     console.log('Succesfully connected to the database server!'.green.inverse.bold);
 }
